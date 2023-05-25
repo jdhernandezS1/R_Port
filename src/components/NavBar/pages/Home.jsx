5e5542444519137d5dfb74a8af0abb973de61b66
David <48605281+jdhernandezS1@users.noreply.github.com>
2023-05-25 15:58:47 +0000

    Resume and Home section

--- src/components/NavBar/pages/Home.jsx
+++ src/components/NavBar/pages/Home.jsx
@@ -1,4 +1,9 @@
+import styles from "../../style/content.module.css";
+
 const Home = () => {
+  let Content = styles.content;
+  let col2 = styles.contentCol2;
+
   return (
     <>
       <h1 className='MainTitle'>
@@ -8,6 +13,126 @@ const Home = () => {
         <picture>
           <img className='ProfilePic' src="https://avatars.githubusercontent.com/u/48605281?v=4" alt="David Profile foto" />
         </picture>
+      </div>
+      <div className={Content}>
+
+        <div>
+          <h1>Software Skills</h1>
+          <ul>
+            <li>
+              <h3> Programming Languages</h3>
+              <img src="https://img.shields.io/badge/C%2B%2B-00599C?style=for-the-badge&logo=c%2B%2B&logoColor=white" alt="C++ icon" />
+              <img src="https://img.shields.io/badge/Python-14354C?style=for-the-badge&logo=python&logoColor=white" alt="Python icon" />
+              <img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white" alt="HTML5" />
+              <img src="https://img.shields.io/badge/CSS-239120?&style=for-the-badge&logo=css3&logoColor=white" alt="CSS3" />
+              <img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black" alt="JavaScript" />
+              <img src="https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=openjdk&logoColor=white" alt="Java" />
+            </li>
+            <li>
+
+              <h3> Frameworks </h3>
+              <img src="https://img.shields.io/badge/-ReactJs-61DAFB?logo=react&logoColor=white&style=for-the-badge" alt="React" />
+              <img src="https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white" alt="Bootstrap" />
+              <img src="https://img.shields.io/badge/Django-092E20?style=for-the-badge&logo=django&logoColor=white" alt="Django" />
+              <img src="https://img.shields.io/badge/Flask-000000?style=for-the-badge&logo=flask&logoColor=white" alt="Flask" />
+
+            </li>
+            <li>
+
+              <h3>Databases </h3>
+              <img src="https://img.shields.io/badge/PostgreSQL-316192?style=for-the-badge&logo=postgresql&logoColor=white" alt="Postgresql" />
+              AWS
+              <img src="https://shields.io/badge/MySQL-lightgrey?logo=mysql&style=plastic&logoColor=white&labelColor=blue" alt="MySQL" />
+            </li>
+            <li>
+
+              <h3>
+                Deployment Platforms
+              </h3>
+              <img src="https://img.shields.io/badge/Heroku-430098?style=for-the-badge&logo=heroku&logoColor=white" alt="Heroku" />
+            </li>
+            <li>
+              <h3>
+                API
+              </h3>
+              <img src="https://img.shields.io/badge/Stripe-626CD9?style=for-the-badge&logo=Stripe&logoColor=white" alt="Stripe" />
+              <img src="https://img.shields.io/badge/Gspread-626CD9?style=for-the-badge&logo=Google&logoColor=white" alt="Gspread" />
+            </li>
+
+            <li>
+              <h3>
+                Control Version
+              </h3>
+              <img src="https://img.shields.io/badge/Git%20-%23302F2F.svg?&style=for-the-badge&logo=Git&logoColor=F05032" alt="Git" />
+              <img src="https://img.shields.io/badge/GitHub%20-%23181717.svg?&style=for-the-badge&logo=GitHub&logoColor=FFFFFF" alt="GitHub" />
+            </li>
+            <li>
+              <h3>
+                Operative Systems
+
+              </h3>
+              <img src="https://img.shields.io/badge/Linux-FCC624?style=for-the-badge&logo=linux&logoColor=black" alt="Linux" />
+              <img src="https://img.shields.io/badge/Windows-0078D6?style=for-the-badge&logo=windows&logoColor=white" alt="Windows" />
+
+            </li>
+          </ul>
+        </div>
+        <div className={col2}>
+          <h2>
+            About Me
+          </h2>
+          <ul>
+            <li>
+              <h3>
+                What do I do?
+              </h3>
+              <p>
+                Skilled Engineer specialized in Full Stack Development with diverse life and technical experience.
+                Expertise in frontend and backend technologies, delivering seamless web applications. Unique perspective and problem-solving abilities.
+                Strong collaboration and communication skills. Passionate about staying up-to-date with industry trends and incorporating cutting-edge technologies.
+                Ready to contribute to a dynamic organization.
+              </p>
+
+            </li>
+            <li>
+              <h3>
+                Why hire me?
+              </h3>
+              <p>
+                <strong>
+                  Hardworking and Dedicated: </strong> I am committed to putting in the effort required to deliver high-quality results. I take pride in my work and consistently strive for excellence.
+
+                Full Stack Proficiency: With expertise across the entire technology stack, I am capable of handling projects from end to end. This allows for seamless integration and efficient development.
+              </p>
+              <p>
+                <strong> Effective Communication: </strong>  I excel in communication and thrive in collaborative, vibrant teams. I believe that open and clear communication is essential for successful project execution.
+              </p>
+              <p>
+
+                <strong> Self-Motivated and Independent:</strong> While I thrive in team environments, I am also a self-starter when working independently. I am proactive in finding solutions and taking initiative to overcome obstacles.
+              </p>
+              <p>
+
+                <strong> Optimistic Problem Solver:</strong> I approach challenges with a positive mindset and firmly believe that every obstacle has a solution. I enjoy tackling problems head-on and finding creative ways to resolve them.
+              </p>
+              <p>
+
+                <strong> Adaptability and Learning:</strong> I am passionate about staying current with the latest industry trends and technologies. I am quick to adapt to new tools and methodologies, ensuring that I am always at the forefront of innovation.
+              </p>
+              <p>
+
+                <strong>Strong Work Ethic:</strong> I bring a strong work ethic to every project, demonstrating a high level of professionalism, reliability, and accountability. I am committed to meeting deadlines and exceeding expectations.
+              </p>
+            </li>
+            <li>
+              <h3>
+                How do I do it?
+              </h3>
+              I begin planning and following a schematic as function. I know that the philosophies of effective design combined with application rate, security and reliability combining to reach a great user experience and performance.
+            </li>
+          </ul>
+
+        </div>
       </div>
     </>
   );
--- src/components/NavBar/pages/Resume.jsx
+++ src/components/NavBar/pages/Resume.jsx
@@ -1,6 +1,15 @@
+import { Fragment } from "react"
+import styles from "../../style/content.module.css";
+import { IoIosPerson, IoMdNavigate, IoIosCall, IoIosMail } from "react-icons/io";
+
 const Resume = () => {
+  let Content = styles.content;
+  let list = styles.list;
+  let Icon = styles.icon;
+  let col2 = styles.contentCol2;
+
   return (
-    <>
+    < >
 
       <h1 className='MainTitle'>
         Resume
@@ -9,6 +18,89 @@ const Resume = () => {
         <picture>
           <img className='ProfilePic' src="https://avatars.githubusercontent.com/u/48605281?v=4" alt="David Profile foto" />
         </picture>
+      </div>
+      <div className={Content}>
+        <div>
+
+
+          <h2>Personal Information</h2>
+          <ul className={list}>
+            <li>
+              <IoIosPerson className={Icon} />  Julio David Hernandez Segura
+            </li>
+            <li>
+              <IoMdNavigate className={Icon} />   6516 Cugnasco, TI, Switzerland
+            </li>
+            <li>
+              <IoIosCall className={Icon} />  +41 (0) 79 558 08 92
+            </li>
+            <li>
+              <IoIosMail className={Icon} />    david-hd97@hotmail.com
+            </li>
+          </ul>
+        </div>
+
+        <div className={col2}>
+          <div >
+            <h1 >
+              Studies
+            </h1>
+          </div>
+          <div data-dates="03/2022 - 03/2023">
+            <h3>Code institute</h3>
+            <p>Full Stack Software Developer </p>
+            <a
+              href="https://www.credential.net/7baa32c1-921d-4399-8881-dec2a782d485#gs.v65mt7">Credential</a>
+          </div>
+          <div data-dates="08/2021 - 02/2022">
+            <h3>City Language School</h3>
+            <p> English course </p>
+          </div>
+          <div data-dates="2020">
+            <h3>Coursera</h3>
+            <p> Programming Foundations with JavaScript, HTML and CSS</p>
+            <a href="https://coursera.org/share/387155c21018c6e0a257a1e1b3e66b4f">Credential</a>
+          </div>
+          <div data-dates="2020">
+            <h3>Coursera</h3>
+            <p> Introduction to self-driving cars</p>
+            <a
+              href="https://www.coursera.org/account/accomplishments/certificate/A6RQTWZJGP3S">Credential</a>
+          </div>
+
+          <div data-dates="02/2015- 08/2020">
+            <h3>Universidad Del Quindio (High Quality certification)</h3>
+            <p>Electronic Engineer</p>
+            <a href="https://wallet.xertify.co/certificates/FFF45B0AA001">Credential</a>
+          </div>
+          <div data-dates="2007 - 2012">
+            <h3>Zakurayima</h3>
+            <p>High School</p>
+          </div>
+          <div data-dates="2000 - 2006">
+            <h3>Los angeles</h3>
+            <p>School</p>
+          </div>
+        <hr />
+        <div >
+          <h1 >
+            Work storie
+          </h1>
+        </div>
+          <div data-dates="07/2022 - 10/2022">
+            <h3>Charme Hotel Torchio</h3>
+            <p>House Keeper </p>
+          </div>
+          <div data-dates="08/2021 - 02/2022">
+            <h3>Barnacles Hostel Dublin</h3>
+            <p> Porter Linnen</p>
+          </div>
+          <div data-dates="02/2016- 06/2019">
+            <h3>Universidad Del Quindio </h3>
+            <p>Logistic and Recreation</p>
+          </div>
+
+        </div>
       </div>
     </>
   );
