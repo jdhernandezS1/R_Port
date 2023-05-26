18aa3e44a4e2b3b7e2f8e83a4ad29e5a355d2841
David <48605281+jdhernandezS1@users.noreply.github.com>
2023-05-26 09:22:46 +0000

    Fixing Route Page

--- src/components/NavBar/Router.jsx
+++ src/components/NavBar/Router.jsx
@@ -3,18 +3,18 @@ import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Resume from "./pages/Resume";
 import Portafolio from "./pages/Project";
-// import Contact from "./pages/Contact";
+import Contact from "./pages/Contact";
 import NoPage from "./pages/NoPage";
 
 const NavBar = () =>{
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="R_Port/" element={<Layout />}>
+        <Route path="R_Port/index.html/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="resume" element={<Resume />} />
           <Route path="portafolio" element={<Portafolio />} />
-          {/* <Route path="contact" element={<Contact />} /> */}
+          <Route path="contact" element={<Contact />} />
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
